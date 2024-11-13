9cef01c264ab4faf1e67c03a9d61028daf74a300
ikrgaurav <kkr2810@gmail.com>
2024-11-13 15:13:09 +0530

    tab

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Set the favicon */}
+        <link rel="icon" href="/nexgen.ico" />
+        {/* Add a default title */}
+        <title>NexGen Ortho</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
